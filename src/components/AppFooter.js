e3e6f048ecec61758e48b78eff5b035670c6a222
Dev-DavidPrabu <davidprabu02@gmail.com>
2021-11-17 11:08:00 +0530

    Layout Style Changes Correction Complete

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -6,16 +6,16 @@ const AppFooter = () => {
     <CFooter>
       <div>
         <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
+          
         </a>
-        <span className="ms-1">&copy; 2021 Exciteon.</span>
+        <span className="ms-1">&copy; Naga Ltd.</span>
       </div>
-      <div className="ms-auto">
+      {/* <div className="ms-auto">
         <span className="me-1">Powered by</span>
         <a href="https://exciteon.com" target="_blank" rel="noopener noreferrer">
           Exciteon.com
         </a>
-      </div>
+      </div> */}
     </CFooter>
   )
 }
